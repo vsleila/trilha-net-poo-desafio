493e7b4b5b78dbab562fa4214e7b0245032902e5
Leila Vasconcelos <leila.vasconcelos.022@gmail.com>
2023-12-05 20:40:11 -0300

    Repositório Resolvido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia: ");
+Smartphone nokia = new Nokia(numero: "9999-9999", modelo: "Nokia G11", imei: "151515151515151", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Duolingo");
+
+Console.WriteLine("Iphone: ");
+Smartphone iphone = new Iphone(numero: "7777-7777", modelo: "Iphone 14", imei: "212121212121212", memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Twitter");
